a51ffcdf937fb4dd4568eb1035c48619d147f79a
aleksandr churilin <alex77bel@yandex.ru>
2023-04-18 22:25:25 +0300

    - Sql-код для создания таблиц `employees`,`customers`,`orders` скопирован в файл `create_tables.sql`
    - код в `main.py` заполняет созданные таблицы данными из файлов папки `north_data`

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+
+
+CREATE TABLE employees
+(
+    employee_id smallserial PRIMARY KEY NOT NULL,
+    first_name  varchar(100)            NOT NULL,
+    last_name   varchar(100)            NOT NULL,
+    title       varchar(100)            NOT NULL,
+    birth_date  date                    NOT NULL,
+    notes       text
+);
+
+CREATE TABLE customers
+(
+    customer_id  varchar(5) PRIMARY KEY NOT NULL,
+    company_name varchar(100)           NOT NULL,
+    contact_name varchar(100)           NOT NULL
+)
+
+CREATE TABLE orders
+(
+    order_id    smallint PRIMARY KEY NOT NULL,
+    customer_id varchar(5) REFERENCES customers (customer_id),
+    employee_id smallint REFERENCES employees (employee_id),
+    order_date  date                 NOT NULL,
+    ship_city   varchar(100)         NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+from csv import reader
+import psycopg2
+
+
+def csv_reader(filename: str) -> list[tuple]:
+    """
+    Получает данные из файла filename, возвращает список кортежей
+    """
+    data = []
+    try:
+        with open(filename, encoding='utf-8') as csvfile:
+            for row in [*reader(csvfile)]:
+                data.append(tuple(row))
+    except FileNotFoundError:
+        raise FileNotFoundError(f'Отсутствует файл {filename}')
+    return data[1:]  # возвращаем данные без заголовка
+
+
+con_params = dict(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="0112"
+)
+
+with psycopg2.connect(**con_params) as con:
+    with con.cursor() as cur:
+        # Заполняем таблицу 'employees'
+        employees_from_csv = csv_reader('./north_data/employees_data.csv')
+        cur.executemany("INSERT INTO employees VALUES (default,%s,%s,%s,%s,%s)", employees_from_csv)
+
+        # Заполняем таблицу 'customers'
+        customers_from_csv = csv_reader('./north_data/customers_data.csv')
+        cur.executemany("INSERT INTO customers VALUES (%s,%s,%s)", customers_from_csv)
+
+        # Заполняем таблицу 'orders'
+        orders_from_csv = csv_reader('./north_data/orders_data.csv')
+        cur.executemany("INSERT INTO orders VALUES (%s,%s,%s,%s,%s)", orders_from_csv)
+
+con.close()
