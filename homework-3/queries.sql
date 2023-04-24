b6b5c8376b99939ac4c1860136c849c956acb456
aleksandr churilin <alex77bel@yandex.ru>
2023-04-24 21:36:29 +0300

    - Написаны sql-запросы на JOIN и подзапросы

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,37 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT c.company_name AS customer, first_name||' '||last_name AS empoloyee
+FROM orders AS o
+JOIN employees AS e USING (employee_id)
+JOIN customers AS c USING (customer_id)
+JOIN shippers AS s ON s.shipper_id = o.ship_via
+WHERE c.city = 'London' and e.city = 'London' and s.company_name = 'United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products
+JOIN suppliers USING (supplier_id)
+JOIN categories USING (category_id)
+WHERE discontinued = 0 and units_in_stock < 25 and category_name in ('Dairy Products', 'Condiments')
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name
+FROM customers
+LEFT JOIN orders USING (customer_id)
+WHERE order_id is NULL
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT product_name
+FROM products
+WHERE product_id = ANY (SELECT product_id
+					    FROM order_details
+					    WHERE quantity = 10)
